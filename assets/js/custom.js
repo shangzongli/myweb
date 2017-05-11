dd03cb866388f9fe032d5c078ae75a8d3f3d53f4
szl07054121 <1272511813@qq.com>
2017-05-12 01:24:25 +0800

    first commit

---
+++ assets/js/custom.js
@@ -0,0 +1,47 @@
+﻿
+
+(function ($) {
+    "use strict";
+    var mainApp = {
+        main_fun: function () {
+            /*实现平滑滚动*/
+            $(function () {
+                $('.szl a').bind('click', function (event) {
+                    var $anchor = $(this);
+                    $('html, body').animate({
+                        scrollTop: $($anchor.attr('href')).offset().top
+                    },1000);
+                });
+            });
+            /*====================================
+            技能部分
+           ======================================*/
+            $(function() {
+                $('.myStat').circliful();
+            });
+
+            /*====================================
+            个人资料部分  提示
+          ======================================*/
+            $(function () {
+                $('a[title]').tooltip();
+            });
+            /*====================================
+            wow动画效果
+            ======================================*/
+            new WOW().init();
+        },
+
+        initialization: function () {
+            mainApp.main_fun();
+
+        }
+
+    }
+    // Initializing //
+
+    $(document).ready(function () {
+        mainApp.main_fun();
+    });
+
+}(jQuery));
---
+++ assets/js/styleSwitcher.js
@@ -0,0 +1,31 @@
+﻿/*主题切换*/
+
+(function () {
+    "use strict";
+    $(document).ready(function () {
+        mainAppNew.main_fun();
+    });
+    var mainAppNew ={
+        main_fun: function () {
+            $('#switch-panel').click(function () {
+            if ($(this).hasClass('show-panel')) {
+                $('.switcher').css({'left': '-50px'});
+                $('#switch-panel').removeClass('show-panel');
+                $('#switch-panel').addClass('hide-panel');
+            } else if ($(this).hasClass('hide-panel')) {
+                $('.switcher').css({'left': 0});
+                $('#switch-panel').removeClass('hide-panel');
+                $('#switch-panel').addClass('show-panel');
+            }
+        });
+
+        $('#pink').click(function () {
+            $('#mainCSS').attr('href', 'assets/css/themes/pink.css');
+        });
+        $('#blue').click(function () {
+            $('#mainCSS').attr('href', 'assets/css/themes/blue.css');
+        });
+    }
+}
+}());
+
